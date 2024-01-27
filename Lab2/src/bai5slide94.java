fa0a63864e528c0edbc01ead07a8c7fbb0f84bcd
PhamViettAnh <vanh1332004@gmail.com>
2024-01-27 17:25:27 +0700

    Lab2

---
+++ Lab2/src/bai5slide94.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class bai5slide94 {
+        public static void main(String[]arg){
+        int n, Tong = 0;
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.print("nhap vao cac so nguyen = ");
+            n = sc.nextInt();
+            Tong += n;
+        }while(Tong<=100);
+        System.out.println("Tong cac so nguyen=" +Tong);
+    }
+}
